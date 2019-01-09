6b68972813ebf8c2bd00b8b250cf69b0b77654fd
sy <s@mewy.pw>
2019-01-08 22:26:37 -0800

    fatty commit

---
+++ scripts/linux.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Neovim
+sudo apt install -y python-neovim python3-neovim python3-pip python-pip neovim
+sudo update-alternatives --install /usr/bin/vi vi /usr/bin/nvim 60
+sudo update-alternatives --config vi
+sudo update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60
+sudo update-alternatives --config vim
+sudo update-alternatives --install /usr/bin/editor editor /usr/bin/nvim 60
+sudo update-alternatives --config editor
+
